471d89409a533efc97e1897c6e9a97e0758028a2
Evgeny Kuryatov <gerakl00@gmail.com>
2022-06-12 17:42:16 +0300

    Fix refactor

--- CG_Lab_1/Framework/InputSystem/InputListener.h
+++ CG_Lab_1/Framework/InputSystem/InputListener.h
@@ -2,42 +2,42 @@
 
 #include "InputCodes.h"
 
-	struct MouseEvent
-	{
-		int x;				
-		int y;
-		MouseEvent(int new_x, int new_y) :  x(new_x), y(new_y) {}
-	};
+struct MouseEvent
+{
+	int x;				
+	int y;
+	MouseEvent(int new_x, int new_y) :  x(new_x), y(new_y) {}
+};
 
 
-	struct MouseEventClick : public MouseEvent
-	{
-		const eMouseKeyCodes button;
-		MouseEventClick(eMouseKeyCodes pressed_button, int x, int y) : MouseEvent(x,y), button(pressed_button) {}
-	};
+struct MouseEventClick : public MouseEvent
+{
+	const eMouseKeyCodes button;
+	MouseEventClick(eMouseKeyCodes pressed_button, int x, int y) : MouseEvent(x,y), button(pressed_button) {}
+};
 
 
-	struct MouseEventWheel : public MouseEvent
-	{
-		int wheel;
-		MouseEventWheel(int new_wheel, int x, int y) : MouseEvent(x,y), wheel(new_wheel) {}
-	};
+struct MouseEventWheel : public MouseEvent
+{
+	int wheel;
+	MouseEventWheel(int new_wheel, int x, int y) : MouseEvent(x,y), wheel(new_wheel) {}
+};
 
 
-	struct KeyEvent
-	{
-		const wchar_t wc;
-		const eKeyCodes code;
-		KeyEvent(wchar_t c, eKeyCodes kc) : wc(c), code(kc) {}
-	};
+struct KeyEvent
+{
+	const wchar_t wc;
+	const eKeyCodes code;
+	KeyEvent(wchar_t c, eKeyCodes kc) : wc(c), code(kc) {}
+};
 
-	class InputListener
-	{
-	public:
-		virtual bool MousePressed(const MouseEventClick &arg) { return false; }
-		virtual bool MouseReleased(const MouseEventClick &arg) { return false; }
-		virtual bool MouseWheel(const MouseEventWheel &arg) { return false; }
-		virtual bool MouseMove(const MouseEvent &arg) {return false; }
-		virtual bool KeyPressed(const KeyEvent &arg) { return false; }
-		virtual bool KeyReleased(const KeyEvent &arg) { return false; }
-	};
+class InputListener
+{
+public:
+	virtual bool MousePressed(const MouseEventClick &arg) { return false; }
+	virtual bool MouseReleased(const MouseEventClick &arg) { return false; }
+	virtual bool MouseWheel(const MouseEventWheel &arg) { return false; }
+	virtual bool MouseMove(const MouseEvent &arg) {return false; }
+	virtual bool KeyPressed(const KeyEvent &arg) { return false; }
+	virtual bool KeyReleased(const KeyEvent &arg) { return false; }
+};
